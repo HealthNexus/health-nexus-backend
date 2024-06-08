b47b6821f100a9565072530b022365b6b9fcad41
anamboijohn <anamboijohn@gmail.com>
2024-06-08 20:07:01 +0000

    create post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->unique()->sentence(),
+            'excerpt' => fake()->paragraph(),
+            'body' => fake()->text(),
+            'thumbnail' => fake()->unique()->word(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
