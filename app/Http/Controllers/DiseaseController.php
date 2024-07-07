0417432c6c394efeabd4e078c2cd9e0848f9b460
anamboijohn <anamboijohn@gmail.com>
2024-07-07 22:31:25 +0000

    add diseases entity with it resource files

---
+++ app/Http/Controllers/DiseaseController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Disease;
+use Illuminate\Http\Request;
+
+class DiseaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Disease $disease)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Disease $disease)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Disease $disease)
+    {
+        //
+    }
+}
