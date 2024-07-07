beca603dca1d87b7c43586bd632b2d189611c3b0
anamboijohn <anamboijohn@gmail.com>
2024-07-07 22:31:04 +0000

    add post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -20,8 +20,8 @@ class PostFactory extends Factory
     {
         return [
             'title' => fake()->unique()->sentence(),
-            'excerpt' => fake()->paragraph(),
-            'body' => fake()->text(),
+            'excerpt' => fake()->paragraph(1),
+            'body' => fake()->paragraph(10),
             'thumbnail' => fake()->unique()->word(),
             'created_at' => now(),
             'updated_at' => now(),
