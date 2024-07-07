444cde7b096e6c106161c7c822bb634044fbc837
anamboijohn <anamboijohn@gmail.com>
2024-07-07 23:22:33 +0000

    transfer query code from previous codebase

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,57 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query
+            ->when(
+                $filters['search'] ?? false,
+                fn ($query, $search) =>
+                $query->where(
+                    fn ($query) =>
+                    $query->where('title', 'LIKE', '%' . request('search') . '%')
+                        ->orWhere('keywords', 'LIKE', '%' . request('search') . '%')
+                        ->orWhere('description', 'LIKE', '%' . request('search') . '%')
+                        ->orWhere('location', 'LIKE', '%' . request('search') . '%')
+                )
+
+            );
+
+        $query
+            ->when(
+                $filters['category'] ?? false,
+                fn ($query, $category) =>
+                $query
+                    ->whereHas(
+                        'category',
+                        fn ($query) =>
+                        $query->where('slug', $category)
+                    )
+            );
+
+        $query
+            ->when(
+                $filters['campus'] ?? false,
+                fn ($query, $campus) =>
+                $query
+                    ->whereHas(
+                        'campus',
+                        fn ($query) =>
+                        $query->where('slug', $campus)
+                    )
+            );
+
+        $query
+            ->when(
+                $filters['creator'] ?? false,
+                fn ($query, $author) =>
+                $query
+                    ->whereHas(
+                        'author',
+                        fn ($query) =>
+                        $query->where('username', $author)
+                    )
+            );
+    }
 }
