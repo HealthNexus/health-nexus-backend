51f83fb6a4c4464ecd3f82166d31a240ec456d4e
anamboijohn <anamboijohn@gmail.com>
2024-07-16 17:00:40 +0000

    define relationship between user and hospital

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function hospital()
+    {
+        return $this->belongsTo(Hospital::class);
+    }
 }
