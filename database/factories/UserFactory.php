d6290ef988c513ec3433351cefe93eb190714cff
anamboijohn <anamboijohn@gmail.com>
2024-07-16 17:02:04 +0000

    add role, hospital_id and disease_id field to userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'role' => 'user',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'hospital_id' => 1,
+            'disease_id' => 1,
         ];
     }
 
