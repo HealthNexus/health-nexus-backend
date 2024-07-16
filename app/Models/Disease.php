fd92a905ab25e2cd09d086c4a05e331e59cbb038
anamboijohn <anamboijohn@gmail.com>
2024-07-16 18:45:45 +0000

    create relationship between user and disease in disease model

--- app/Models/Disease.php
+++ app/Models/Disease.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Disease extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function patients(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'disease_user', 'disease_id', 'user_id');
+    }
 }
