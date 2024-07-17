d2e33ae6c82ebf6b178c11b40a27e4acd2e3686c
anamboijohn <anamboijohn@gmail.com>
2024-07-17 15:21:28 +0000

    add primary key 'id' to disease_user table

--- database/migrations/2024_07_16_171419_create_disease_user_table.php
+++ database/migrations/2024_07_16_171419_create_disease_user_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('disease_user', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('disease_id')->constrained();
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
