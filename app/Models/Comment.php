fad8c02d1d76a65b5315b74b970e490d058a3687
anamboijohn <anamboijohn@gmail.com>
2024-07-18 11:38:33 +0000

    refactor code

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,17 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
-    public function replies(){
+    //relationship between commentss and replies
+    public function replies(): HasMany
+    {
         return $this->hasMany(Reply::class);
     }
 
-    public function post(){
+    //relationship between comments and post
+    public function post():BelongsTo
+    {
         return $this->belongsTo(Post::class);
+    }
+
+    //relationship between comments and user
+    public function writer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
