6c34ac8b12c0d77b8819a5d7cf32f5149f3c052d
anamboijohn <anamboijohn@gmail.com>
2024-07-23 11:11:54 +0000

    eager load relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,7 +13,7 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
-    protected $with = ['writer', 'comments'];
+    protected $with = ['writer', 'comments', 'disease'];
 
     public function scopeFilter($query, array $filters)
     {
