bdc8a843a7010f7530c75b9989e7f00cc8509154
anamboijohn <anamboijohn@gmail.com>
2024-07-24 13:10:08 +0000

    fetch drugs

--- app/Http/Controllers/DrugController.php
+++ app/Http/Controllers/DrugController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Drug;
 use Illuminate\Http\Request;
 
 class DrugController extends Controller
 {
-    //
+    public function index()
+    {
+        if (auth()->user()->role->slug == 'admin' || auth()->user()->role->slug == 'doctor') {
+            $symptoms = Drug::latest()->get();
+            return response(['drugs' => $symptoms], 200);
+        } else {
+            return response(['message' => 'Unauthorized'], 401);
+        }
+    }
 }
