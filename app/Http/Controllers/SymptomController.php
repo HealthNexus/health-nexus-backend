02899285b71a7c302d0b4ff72dd456f78c401374
anamboijohn <anamboijohn@gmail.com>
2024-07-29 12:39:56 +0000

    fetch all symptoms

--- app/Http/Controllers/SymptomController.php
+++ app/Http/Controllers/SymptomController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Symptom;
 use Illuminate\Http\Request;
 
 class SymptomController extends Controller
 {
-    //
+    public function index()
+    {
+        if (auth()->user()->role->slug == 'admin' || auth()->user()->role->slug == 'doctor') {
+            $symptoms = Symptom::latest()->get();
+            return response(['symptoms' => $symptoms], 200);
+        } else {
+            return response(['message' => 'Unauthorized'], 401);
+        }
+    }
 }
