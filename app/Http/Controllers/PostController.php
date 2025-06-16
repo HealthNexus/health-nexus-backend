b8b7112d19395bcc53f06efb8e6ce363a2b4cd43
anamboijohn <anamboijohn@gmail.com>
2025-06-16 15:00:14 +0000

    add NewDiseasePostCreated event and update PostController to fetch latest posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
     public function index()
     {
         //,
-        $posts = Post::all();
+        $posts = Post::latest()->get();
         return response(['posts' => $posts], 200);
     }
 
